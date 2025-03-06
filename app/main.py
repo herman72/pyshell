f51aface59c696a5df84cd323e7c4491d3e24ebe
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-06 10:26:37 +0200

    :hammer: add exit

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
             # Wait for user input
             command = input()
             if command.lower() in ["exit 0"]:
-                print("Exiting shell...")
+                
                 sys.exit(0)
 
             print(f"{command}: command not found")
