dd9a7e9c69df7657940266c276342c351ec08ced
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-06 10:23:42 +0200

    :hammer: add exit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import sys
+
 def main():
-    # Uncomment this block to pass the first stage
-    while True:
-        sys.stdout.write("$ ")
-        # Wait for user input
-        command = input()
-        print(f"{command}: command not found")
+    try:
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()  # Ensure the prompt is displayed immediately
+
+            # Wait for user input
+            command = input()
+            if command.lower() in ["exit 0"]:
+                print("Exiting shell...")
+                break
+
+            print(f"{command}: command not found")
+    except KeyboardInterrupt:
+        print("\nExiting shell...")
+
 if __name__ == "__main__":
     main()
