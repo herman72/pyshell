72ffa660e2f6c94b1b2bde3ee639c0497257deca
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-24 20:37:48 +0200

    add find exe file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import os
 def main():
     input_path = os.environ.get("PATH", "")
     input_path = input_path.split(":") if input_path else []
-    
+    is_found = False
     
     try:
         while True:
@@ -24,13 +24,16 @@ def main():
                     print(f"{command.split()[1]} is a shell builtin")
                 
                 elif input_path is not None:
+        
                     for path in input_path:
                         if os.path.exists(f"{path}/{command.split()[1]}"):
                             print(f"{command.split()[1]} is {path}/{command.split()[1]}")
-                            break       
+                            is_found = True
+                            break
+                    if not is_found:
+                        print(f"{command.split()[1]}: not found")
                 else:
                     print(f"{command.split()[1]}: not found")
-                    break  # Ensure the loop exits after handling the command
             else:
                 print(f"{command}: command not found")
     except KeyboardInterrupt:
