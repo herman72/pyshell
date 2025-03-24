1cf42ea633be10b47b34cc91a0d5532237e205b2
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-24 21:22:29 +0200

    add find exe file and execute command

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ def main():
                         print(f"{arg}: not found")
                 case [arg1, arg2]:
                     print(shutil.which(arg1))
+                    print("helloowwww")
                     print(shutil.which(arg2))
                     if (path1 := shutil.which(arg1)) and (path2 := shutil.which(arg2)):
                         # execute arg1 with arg2 as argument
