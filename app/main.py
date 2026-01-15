49eb42b6995dcc28dcf9ec552718bf73df0c7e70
M. Sherafati <36226207+herman72@users.noreply.github.com>
2026-01-15 20:56:21 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,12 +5,18 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        
         if command.lower() in ["exit"]:
             sys.exit(0)
         if command.lower().split()[0] in ["echo"]:
             # print("/n")
             print(f"{" ".join(command.split()[1:])}")
-        else:
+        if command.lower().split()[0] in ["type"]:
+            if command.lower().split()[1] in ["echo", "exit", "type"]:
+                print(f"{command.lower().split()[1]} is a shell builtin")
+            else:
+                print(f"{command.lower().split()[1]}: not found")
+        if command.lower().split()[0] not in ["echo", "exit", "type"]:
             print(f"{command}: command not found")
 
 
