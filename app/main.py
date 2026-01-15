d24e372ea8e45c76ed8204b2c14798779d51734c
M. Sherafati <36226207+herman72@users.noreply.github.com>
2026-01-15 20:26:51 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
-            break
+        if command.lower() in ["exit 0"]:
+            sys.exit(0)
         print(f"{command}: command not found")
 
 
