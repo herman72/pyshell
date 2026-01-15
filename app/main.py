ae77660c4a5a2f0642324e9eef45b4638ab59d57
M. Sherafati <36226207+herman72@users.noreply.github.com>
2026-01-15 21:02:48 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,23 +2,31 @@ import sys
 
 
 def main():
-    while True:
-        sys.stdout.write("$ ")
-        command = input()
-        
-        if command.lower() in ["exit"]:
-            sys.exit(0)
-        if command.lower().split()[0] in ["echo"]:
-            # print("/n")
-            print(f"{" ".join(command.split()[1:])}")
-        if command.lower().split()[0] in ["type"]:
-            if command.lower().split()[1] in ["echo", "exit", "type"]:
-                print(f"{command.lower().split()[1]} is a shell builtin")
-            else:
-                print(f"{command.lower().split()[1]}: not found")
-        if command.lower().split()[0] not in ["echo", "exit", "type"]:
-            print(f"{command}: command not found")
+    try:
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()  # Ensure the prompt is displayed immediately
 
+            # Wait for user input
+            command = input()
+            if command.lower() in ["exit 0"]:
+                sys.exit(0)
+            elif command.lower() in ["exit 1"]:
+                sys.exit(1)
+            elif command.lower() in ["exit"]:
+                sys.exit(0)
+            elif command.lower().split()[0] == "echo":
+                print(" ".join(command.split()[1:]))
+            elif command.lower().split()[0] == "type":
+                if command.lower().split()[1] in ["exit", "type", "echo"]:
+                    print(f"{command.split()[1]} is a shell builtin")
+                else:
+                    print(f"{command.split()[1]}: not found")
+            else:
+                print(f"{command}: command not found")
+    except KeyboardInterrupt:
+        print("\nExiting shell...")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
