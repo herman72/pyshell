cbfb31827d6c8c8c0b3a126624c88d81e37b2946
M. Sherafati <36226207+herman72@users.noreply.github.com>
2026-01-15 20:38:11 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ def main():
         command = input()
         if command.lower() in ["exit"]:
             sys.exit(0)
-        print(f"{command}: command not found")
+        if command.lower().split()[0] in ["echo"]:
+            # print("/n")
+            print(f"{" ".join(command.split()[1:])}")
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
